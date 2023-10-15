14418f9bcabc27d197b28826ab9a24d578608c0a
Kazeem Jimoh <kazeem0825@gmail.com>
2023-10-15 14:34:53 +0100

    update

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
-
+const cors = require('cors')
 // Middleware
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-// Routes
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// Home page
+app.get('/', (req, res) => {
+    return res.send('Welcome to the street')
+});
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  return res.status(200).json({
+    message: 'User successfully logged in',
+    data: {},
+    error: false
+  })
+});
 
 // Server setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
